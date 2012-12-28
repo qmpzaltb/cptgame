544f609df5dd967576e3d4c9696394abb7c3be65
Nerman <n.nicholas110@gmail.com>
2012-12-28 01:17:48 -0500

    Entity Class Adjustments + Additions
    -We now are using PRIVATE variables :D
    -Additions have comments so check it out and let me know what you think.

--- Dung/src/dungEntity/Entity.java
+++ Dung/src/dungEntity/Entity.java
@@ -2,12 +2,14 @@ package dungEntity;
 
 public class Entity {
 
-	int iEntityID;
+	private int iEntityID;
+	private int iEntityStatus; //This will be health and stuff (40% health remaining and stuff...) The reason why I named this EntityStatus because entities like arrows can be like 50% used (this means arrow is broken and done with) or 50% unused (you haven't shot this arrow yet)
+	//Should we have a variable called EntityCurrAction? and use a coding system (like used in EntityID) to make specific actions like Attack, Heal, Defend, Move, Use Item/Potion, Call Chuck Norris, etc...
 	
-	double dRadius;
-	double dXPos;
-	double dYPos;
-	double dHeading;
+	private double dRadius;
+	private double dXPos;
+	private double dYPos;
+	private double dHeading;
 	EntityController encController;
 	
 	public Entity(int entityID, Entity baseEntity, double xPos, double yPos, double heading){
