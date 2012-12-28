f547c2335ee88f455293bbff26f8f237f1e2d9d8
Nerman <n.nicholas110@gmail.com>
2012-12-28 02:23:57 -0500

    An Early Entity List
    Please add more, but keep in mind the Important Note I left at the
    beginning of the list. Also, lets try to think about this
    entityAnimations package :S

--- Dung/src/dungEntity/Entity.java
+++ Dung/src/dungEntity/Entity.java
@@ -1,7 +1,44 @@
 package dungEntity;
 
 public class Entity {
-
+	
+	/* Entity List. -- This is just to refer to what entity is what and its corresponding code.
+	 * ----------------------------------------------------------------------------------------
+	 * 
+	 * Important Note: Lets not make too many entities... keep in mind we need to make images for these and animations. ALSO IMPORTANT THING! should we make a package for entityAnimations? 
+	 * 
+	 * #### MONSTERS:
+	 *                 Unicorn                                             ---- 0001
+	 *                 Skeleton                                            ---- 0002
+	 *                 Creeper :D:D:D:D:D:D:D:D:D:D:D                      ---- 0003
+	 *                 Robot                                               ---- 0004
+	 *                 Zombie                                              ---- 0005
+	 *                 Dwarf                                               ---- 0006
+	 *                 Gool                                                ---- 0007
+	 *                 Bat                                                 ---- 0008
+	 *                 Wolf                                                ---- 0009
+	 *                 Dog                                                 ---- 0010
+	 *                 Shark                                               ---- 0011
+	 *                 Whale                                               ---- 0012
+	 *                 Pikachu                                             ---- 0013
+	 * #### DUMB: --QUESTION: Can you dual weild? Because if you can do we count 2 swords as a seperate entity or do we combine 2 entities D: I suggest we don't do this because it will complicate things
+	 *                 Bow                                                 ---- 0014
+	 *                 Arrow                                               ---- 0015
+	 *                 Boots                                               ---- 0016
+	 *                 Armor                                               ---- 0017
+	 *                 Caps???????????????? TF2 ANYBODY? :D                ---- 0018
+	 *                 Gun                                                 ---- 0019
+	 *                 Sword                                               ---- 0020
+	 *                 Sheild                                              ---- 0021
+	 *                 Broken Bear Bottle                                  ---- 0022
+	 *                 Unicorn Horn                                        ---- 0023
+	 *                 Bible (Yes Mark, for bonus marks from the teacher we need to add biblic morality) :)
+	 *                 
+	 *                                        
+	 * #### PLAYER:
+	 *                 Current PC Player (If we decide on doing a network) ---- 00?? 
+	 * 
+	 */
 	private int iEntityID;
 	private int iEntityStatus; //This will be health and stuff (40% health remaining and stuff...) The reason why I named this EntityStatus because entities like arrows can be like 50% used (this means arrow is broken and done with) or 50% unused (you haven't shot this arrow yet)
 	private int iEntityAction; //The entity's current action. Try to use the values from GameActions from here.
