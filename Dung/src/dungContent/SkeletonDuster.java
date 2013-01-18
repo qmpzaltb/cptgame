56b2873ea0119c63bb2de7ba4e2930fb68b64628
QMPZALTBUS <mark.b.ftw@gmail.com>
2013-01-18 16:48:09 -0500

    Merging

---
+++ Dung/src/dungContent/SkeletonDuster.java
@@ -0,0 +1,51 @@
+package dungContent;
+
+import dungEntity.AnimationType;
+import dungEntity.EntitySkeleton;
+import dungEntity.SkeletonLimb;
+
+public class SkeletonDuster extends EntitySkeleton {
+
+	
+	public SkeletonDuster(){
+		sklaSkeleton = new SkeletonLimb[] {
+				new LimbLine(3f, 0.0,3.0,  0.0,-12.0), //Limb 0, handle&stock
+				new LimbLine(1f, 0.0,-6.0,  3.0,-4.0), //Limb 1, String, right bottom
+				new LimbLine(1f, 0.0,-8.0,  3.0,-6.0), //Limb 2, String, right bottom-middle
+				new LimbLine(1f, 0.0,-10.0,  3.0,-8.0), //Limb 3, String, right top-middle
+				new LimbLine(1f, 0.0,-12.0,  3.0,-10.0), //Limb 4, String, right top
+				new LimbLine(1f, 0.0,-6.0,  -3.0,-4.0), //Limb 5, String, left bottom
+				new LimbLine(1f, 0.0,-8.0,  -3.0,-6.0), //Limb 6, String, left bottom-middle
+				new LimbLine(1f, 0.0,-10.0,  -3.0,-8.0), //Limb 7, String, left top-middle
+				new LimbLine(1f, 0.0,-12.0,  -3.0,-10.0) //Limb 8, String, left top.
+		};
+		
+		
+	}
+	
+	
+	public void doAnimation(AnimationType animType, long animTime) {
+
+		switch(animType){
+		case IDLE:{
+			int iIdleAnimTime = Math.abs((int)(animTime % 60));
+			double dSwayMagnitude = -1 * Math.cos((2 * Math.PI / 60) * (iIdleAnimTime));
+			sklaSkeleton[1].setDoubleY2(dSwayMagnitude - 4);
+			sklaSkeleton[2].setDoubleY2(dSwayMagnitude - 6);
+			sklaSkeleton[3].setDoubleY2(dSwayMagnitude - 8);
+			sklaSkeleton[4].setDoubleY2(dSwayMagnitude - 10);
+			sklaSkeleton[5].setDoubleY2(dSwayMagnitude - 4);
+			sklaSkeleton[6].setDoubleY2(dSwayMagnitude - 6);
+			sklaSkeleton[7].setDoubleY2(dSwayMagnitude - 8);
+			sklaSkeleton[8].setDoubleY2(dSwayMagnitude - 10);
+			break;
+		}
+		
+		
+		
+		
+		}
+
+	}
+
+}
